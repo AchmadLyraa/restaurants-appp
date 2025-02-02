9931ab56a4d06d329a80b7686de0fde04a1a9893
Achmad Bayhaqi <ahmadbayhaqi069@gmail.com>
2025-02-03 00:11:10 +0800

    change webpack common-1

--- webpack.common.js
+++ webpack.common.js
@@ -2,8 +2,6 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const BundleAnalyzerPlugin =
-	require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
 	entry: {
